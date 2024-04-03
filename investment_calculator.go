1b9fad82bbf33c003d3d97543ee121d157703fe9
Travis Lamberte <travis@lamberte.com>
2024-04-03 10:59:49 -0700

    UPDATE

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
+	const inflationRate float64 = 2.5
 	var investmentAmount float64 = 1000
-	var expectedReturnRate float64 = 5.5
-	var years float64 = 10
+	var years float64 = 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	fmt.Scan(&investmentAmount)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }

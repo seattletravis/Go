44c78b21ea265dd650955c100ceca71eb9cec365
Travis Lamberte <travis@lamberte.com>
2024-04-03 12:35:28 -0700

    refactor calculations into a funtion

--- Investment_Calculator/investment_calculator.go
+++ Investment_Calculator/investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
-	const inflationRate float64 = 2.5
 	var investmentAmount float64 = 1000
 	var years float64 = 10.0
 	var expectedReturnRate float64 = 5.5
@@ -18,8 +19,9 @@ func main() {
 	fmt.Print("Enter time in years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	formattedFutureValue := fmt.Sprintf("Future Value: %.2f\n", futureValue)
 	formattedRealValue := fmt.Sprintf("Future Value (adjusted for inflation): %.2f\n", futureRealValue)
@@ -27,6 +29,18 @@ func main() {
 	// fmt.Printf("Future return: %.2f\nInflation Adjusted Return: %.2f\n", futureValue, futureRealValue)
 	// fmt.Printf("Inflation Adjusted Return: %.0f\n", futureRealValue)
 
-	fmt.Print(formattedFutureValue, formattedRealValue)
+	// fmt.Print(formattedFutureValue, formattedRealValue)
 
+	outputText(formattedFutureValue, formattedRealValue)
+
+}
+
+func outputText(text1, text2 string){
+	fmt.Print(text1, text2)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64){
+	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }

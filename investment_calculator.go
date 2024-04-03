90163e3d4e3a9be4e04c0478a02d8cd6977f8765
Travis Lamberte <travis@lamberte.com>
2024-04-03 09:06:53 -0700

    clean up

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var investmentAmount = 1000
@@ -8,5 +11,5 @@ func main() {
 	var years = 10
 
 	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
-	
+	fmt.Println(futureValue)
 }

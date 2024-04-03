6e72dc8468a22cde5a171a0f52e7e2277fb0b92a
Travis Lamberte <travis@lamberte.com>
2024-04-03 14:45:30 -0700

    add

--- Bank/bank.go
+++ Bank/bank.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
  
 func main(){
+	var accountBalance float64 = 1000
+
+
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
@@ -11,8 +15,30 @@ func main(){
 	fmt.Println("4. Exit")
 
 	var choice int
-	fmt.Print("Your choice: ")
+	fmt.Print("Your choice is: ")
 	fmt.Scan(&choice)
 
-	fmt.Println("Your choice: ", choice)
+	if choice == 1 {
+		fmt.Println("Your Balance is: ", accountBalance)
+	} else if choice == 2 {
+		fmt.Println("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Balance updated! New balance:", accountBalance)
+	}else if choice == 3 {
+		fmt.Println("Withdraw amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+		if (accountBalance - withdrawAmount) < 0 {
+			fmt.Println("Insufficient funds for request")
+			fmt.Println("Balance unchanged:", accountBalance)
+		} else {
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New balance:", accountBalance)
+		}
+	}
+
+
+	fmt.Println("Your choice is: ", choice)
 }

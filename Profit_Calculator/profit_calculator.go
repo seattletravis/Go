d9dc1ac9115be9a18a2a45f5a768cacd45513b14
Travis Lamberte <travis@lamberte.com>
2024-04-03 13:01:49 -0700

    finish up reafactor

--- Profit_Calculator/profit_calculator.go
+++ Profit_Calculator/profit_calculator.go
@@ -4,20 +4,29 @@ import "fmt"
 
 
 func main(){
-	var revenue, expenses, taxRate float64
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter taxRate: ")
-	fmt.Scan(&taxRate)
 
-	var ebt float64 = revenue - expenses
-	var profit float64 = ebt * (1 - taxRate / 100)
-	var ratio float64 = ebt / profit
+	revenue := promptUser("Enter revenue: ")
+	expenses := promptUser("Enter expenses: ")
+	taxRate := promptUser("Enter tax rate: ")
+
+	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
 
+}
+
+func promptUser(text string)(float64){
+	var output float64
+	fmt.Print(text)
+	fmt.Scan(&output)
+	return output
+}
+
+func calculate(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64){
+	ebt = revenue - expenses
+	profit = ebt * (1  -taxRate / 100)
+	ratio = ebt / profit
+	return ebt, profit, ratio
 }

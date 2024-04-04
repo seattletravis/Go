f99e12ab5eaa3cec3d5fa7ef6757b8c60063919c
Travis Lamberte <travis@lamberte.com>
2024-04-04 08:08:01 -0700

    add for loop to go app

--- Bank/bank.go
+++ Bank/bank.go
@@ -8,42 +8,49 @@ func main(){
 
 
 	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
+	
+	for {
 
-	var choice int
-	fmt.Print("Your choice is: ")
-	fmt.Scan(&choice)
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	if choice == 1 {
-		fmt.Println("Your Balance is: ", accountBalance)
-	} else if choice == 2 {
-		fmt.Println("Your deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
+		var choice int
+		fmt.Print("Your choice is: ")
+		fmt.Scan(&choice)
 
-		if depositAmount <= 0 {
-			fmt.Println("Deposit amount must be greater than 0")
-			fmt.Println("Balance unchanged:", accountBalance)
-			return
-		} 
-		accountBalance += depositAmount
-		fmt.Println("Balance updated! New balance:", accountBalance)
-	} else if choice == 3 {
-		fmt.Println("Withdraw amount: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-		if withdrawAmount > accountBalance {
-			fmt.Println("Insufficient funds for request")
-			fmt.Println("Balance unchanged:", accountBalance)
-			return
+		if choice == 1 {
+			fmt.Println("Your Balance is: ", accountBalance)
+		} else if choice == 2 {
+			fmt.Println("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Deposit amount must be greater than 0")
+				fmt.Println("Balance unchanged:", accountBalance)
+				continue
+			} 
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New balance:", accountBalance)
+		} else if choice == 3 {
+			fmt.Println("Withdraw amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount > accountBalance {
+				fmt.Println("Insufficient funds for request")
+				fmt.Println("Balance unchanged:", accountBalance)
+				continue
+			}
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New balance:", accountBalance)
+		} else {
+			fmt.Println("Goodbye!")
+			break
 		}
-		accountBalance -= withdrawAmount
-		fmt.Println("Balance updated! New balance:", accountBalance)
-	} else {
-		fmt.Println("Thanks for banking with us today!")
-	}
+	}	
+	fmt.Println("Thanks for banking with us today!")
+
 }

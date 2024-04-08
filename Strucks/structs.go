0d59f66de379974f2b7bcb7462150d457ca1550f
Travis Lamberte <travis@lamberte.com>
2024-04-08 08:39:43 -0700

    add main func to structs project

--- Strucks/structs.go
+++ Strucks/structs.go
@@ -1,5 +1,20 @@
-package mai
+package main
+
+import "fmt"
 
 func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthDate := getUserData("Please enter your birthdate {MM/DD/YYYY}: ")
 
+
+	fmt.Println(firstName,lastName, birthDate)
 }
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
+

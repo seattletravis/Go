6059ef34f6d9defc67f31e23c136b89b422b4294
Travis Lamberte <travis@lamberte.com>
2024-04-10 07:52:27 -0700

    add struct

--- Strucks/structs.go
+++ Strucks/structs.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+
+type user struct {
+	firstName string
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
+
 
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate {MM/DD/YYYY}: ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate {MM/DD/YYYY}: ")
 
-	// ... Do something with structs!
+	var appUser user
 
+	appUser = user{
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
+	
 	outputUserDetails(firstName, lastName, birthdate)
 
 }

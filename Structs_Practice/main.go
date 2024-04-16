2d87dbf0edc1506cdaa6e489da4e942f301b3280
Travis Lamberte <travis@lamberte.com>
2024-04-16 08:34:39 -0700

    add logic for getting title anc content in main.go

--- Structs_Practice/main.go
+++ Structs_Practice/main.go
@@ -6,6 +6,28 @@ import (
 )
 
 func main(){
+	title, content, err := getNoteData()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	
+
+}
+
+func getNoteData() (string, string, error) {
+	title, err := getUserInput("Note title: ")
+	if err != nil {
+		return "", "", err
+	}
+	content, err := getUserInput("Note content: ")
+
+	if err != nil {
+		return "", "", err
+	}
+	return title, content, nil
 
 }
 

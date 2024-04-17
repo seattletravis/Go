1891c49ddabf2dd0c0f9577c9f1268b7d8f50cdb
Travis Lamberte <travis@lamberte.com>
2024-04-17 08:34:06 -0700

    save form data as json

--- Notes_Structs_Practice/note/note.go
+++ Notes_Structs_Practice/note/note.go
@@ -10,18 +10,18 @@ import (
 )
 
 type Note struct {
-	title string
-	content string
-	createdAt time.Time
+	Title string
+	Content string
+	CreatedAt time.Time
 }
 
 func (note Note) Display() {
-	fmt.Printf("Your note titled %v has the following content:\n\n%v\n\n", note.title, note.content)
+	fmt.Printf("Your note titled %v has the following content:\n\n%v\n\n", note.Title, note.Content)
 }
 
 func (note Note) Save() error {
-	fileName := strings.ReplaceAll(note.title, " ", "_")
-	fileName = strings.ToLower(fileName)
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
 
 	json, err := json.Marshal(note)
 
@@ -38,9 +38,9 @@ func New(title, content string) (Note, error) {
 	}
 
 	return Note{
-		title: title,
-		content: content,
-		createdAt: time.Now(),
+		Title: title,
+		Content: content,
+		CreatedAt: time.Now(),
 	}, nil
 
 } 

45301af5f708ae0a51a74299e7a8d6cb117bbbd2
Travis Lamberte <travis@lamberte.com>
2024-04-17 08:28:16 -0700

    rename structs practice to notes

---
+++ Notes_Structs_Practice/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"example.com/note/note"
+)
+
+func main(){
+	title, content := getNoteData()
+
+	userNote, err := Note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return
+	}
+
+	fmt.Println("Saving the note succeeded!")
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+
+	content := getUserInput("Note content:")
+
+	return title, content
+
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+
+	return text
+
+}

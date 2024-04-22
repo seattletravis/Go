8a32045090934a64ad7d10f3a1e2fb2e658554da
Travis Lamberte <travis@lamberte.com>
2024-04-22 09:22:12 -0700

    working on it.

--- Data_Containers/lists.go
+++ Data_Containers/lists.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main(){
-	var productNames [4]string
+	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99,9.99,45.99,20.0}
-	fmt.Println(prices)
+	fmt.Println(prices, productNames)
+
 }

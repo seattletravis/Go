0209ab0e206e0adb9f7164e4a27a6db06ad73542
Travis Lamberte <travis@lamberte.com>
2024-04-22 09:04:25 -0700

    remove struct

--- Data_Containers/lists.go
+++ Data_Containers/lists.go
@@ -1,11 +1,8 @@
 package main
 
-type Product struct {
-  title string
-  id string
-  price float64
-}
+import "fmt"
 
 func main(){
 	prices := [4]float64{10.99,9.99,45.99,20.0}
+	fmt.Println(prices)
 }

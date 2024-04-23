c6847904c047ed65e0b20073b6ae52d45a006f14
Travis Lamberte <travis@lamberte.com>
2024-04-23 08:43:10 -0700

    give alias to fmt and refactor

--- Data_Containers/lists.go
+++ Data_Containers/lists.go
@@ -1,14 +1,17 @@
 package main
 
-import "fmt"
+import f "fmt"
 
 func main(){
 	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99,9.99,45.99,20.0}
-	fmt.Println(prices, productNames)
+	f.Println(prices, productNames)
 
 	productNames[2] = "A Carpet"
 
-	fmt.Println(prices, productNames)
+	f.Println(prices, productNames)
 
+	featuredPrices := prices[1:3]
+
+	f.Println(featuredPrices)
 }

7260db726ddbc5b72dc76bd8afe88d291d93b44e
Travis Lamberte <travis@lamberte.com>
2024-04-23 08:31:59 -0700

    change list item 2

--- Data_Containers/lists.go
+++ Data_Containers/lists.go
@@ -7,7 +7,8 @@ func main(){
 	prices := [4]float64{10.99,9.99,45.99,20.0}
 	fmt.Println(prices, productNames)
 
+	productNames[2] = "A Carpet"
 
-	fmt.Println(prices[2])
+	fmt.Println(prices[2], productNames)
 
 }

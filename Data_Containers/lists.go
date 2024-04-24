429c1a4d267b1e014088a47bb03c67138d1fe47f
Travis Lamberte <travis@lamberte.com>
2024-04-24 09:43:25 -0700

    refactor

--- Data_Containers/lists.go
+++ Data_Containers/lists.go
@@ -2,16 +2,23 @@ package main
 
 import f "fmt"
 
-func main(){
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99,9.99,45.99,20.0}
-	f.Println(prices, productNames)
+func main () {
+	prices := []float64{10.99, 8.99}
+	f.Println(prices[1])
 
-	productNames[2] = "A Carpet"
-
-	f.Println(prices, productNames)
-
-	featuredPrices := prices[:3]
-
-	f.Println(featuredPrices)
 }
+
+
+// func main(){
+// 	var productNames [4]string = [4]string{"A Book"}
+// 	prices := [4]float64{10.99,9.99,45.99,20.0}
+// 	f.Println(prices, productNames)
+
+// 	productNames[2] = "A Carpet"
+
+// 	f.Println(prices, productNames)
+
+// 	featuredPrices := prices[:3]
+
+// 	f.Println(featuredPrices)
+// }

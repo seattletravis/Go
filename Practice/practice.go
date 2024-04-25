6b4bdbd75ae15e55c81ef4be5881f3f292ab31b4
Travis Lamberte <travis@lamberte.com>
2024-04-25 09:43:10 -0700

    add slices

--- Practice/practice.go
+++ Practice/practice.go
@@ -8,6 +8,12 @@ func main () {
 	var hobbies = [3]string{"landscaping", "cooking", "woodworking"}
 	f.Println(hobbies)
 
+	element1 := hobbies[0]
+	f.Println(element1)
+	element2 := hobbies[1:]
+	f.Println(element2)
+	f.Println(hobbies[:2])
+
 }
 
 // Time to practice what you learned!

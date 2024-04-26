faaaa3254360e09484ced2522eebf070dd48db16
Travis Lamberte <travis@lamberte.com>
2024-04-26 06:53:12 -0700

    delete entry in map

--- Data_Containers/maps.go
+++ Data_Containers/maps.go
@@ -11,4 +11,9 @@ func main(){
 
 	f.Println(websites)
 	f.Println(websites["Amazon"])
+	websites["LinkedIn"] = "linkedin.com"
+	f.Println(websites)
+
+	delete(websites, "Google")
+	f.Println(websites)
 }

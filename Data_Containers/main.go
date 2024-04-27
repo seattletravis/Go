c859b5efbd72b890d3a6385e45a67e91e19258dc
Travis Lamberte <travis@lamberte.com>
2024-04-27 07:57:48 -0700

    use make fucntion on a map

--- Data_Containers/main.go
+++ Data_Containers/main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	userNames := make([]string, 2)
+	userNames := make([]string, 2, 5)
 
 	userNames[0] = "Juwel"
 
 	userNames = append(userNames, "Matt", "Max")
 
 	fmt.Println(userNames)
+
+	courseRatings := map[string]float64{}
+
+	courseRatings["go"] = 4.7
+	courseRatings["react"] = 4.8
+
 }

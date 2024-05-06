398ec17d34188fe079286b438a6a97a77cbffaa2
Travis Lamberte <travis@lamberte.com>
2024-05-06 07:51:45 -0700

    add function to project

--- Functions/main.go
+++ Functions/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	numbers := []int{1, 2, 3, 4}
+	double := doubleNumbers(&numbers)
+	fmt.Println(double)
+}
+
+func doubleNumbers(numbers *[]int) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, val*2)
+	}
+
+	return dNumbers
 }

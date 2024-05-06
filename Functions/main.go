17649940e11f6b658cf07a2ce9c8480b8bfc1396
Travis Lamberte <travis@lamberte.com>
2024-05-06 07:55:03 -0700

    add double function

--- Functions/main.go
+++ Functions/main.go
@@ -11,8 +11,12 @@ func main() {
 func doubleNumbers(numbers *[]int) []int {
 	dNumbers := []int{}
 	for _, val := range *numbers {
-		dNumbers = append(dNumbers, val*2)
+		dNumbers = append(dNumbers, double(val))
 	}
 
 	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
 }

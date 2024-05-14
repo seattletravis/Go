eb6b9114a00c2360b2908e21d4c7b697a02a5eec
Travis Lamberte <travis@lamberte.com>
2024-05-13 19:54:59 -0700

    add sumup function

--- Functions/main.go
+++ Functions/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	numbers := []int{1, 2, 3, 5, 4, 5, 6}
+
+	summary := sumup(numbers)
+
+	fmt.Println(summary)
+
+}
+
+func sumup(numbers []int) int {
+	sum := 0
+
+	for _, val := range numbers {
+		sum += val
+	}
+
+	return sum
 }

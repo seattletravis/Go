fa0bc649effa0da72af1c0bf5cf60a6e67a6f039
Travis Lamberte <travis@lamberte.com>
2024-05-13 19:40:40 -0700

    recursion

--- Functions/main.go
+++ Functions/main.go
@@ -8,15 +8,17 @@ func main() {
 
 }
 
+func factorial(number int) int {
+	if number == 0{
+		return 1
+	}
+	return number * factorial(number - 1)
 
-
-
-// func factorial(number int) int {
-// 	result := 1
-// 	for i := 1; i <= number; i++ {
-// 		result = result * i
-// 	}
-// 	return result
-// }
+	// result := 1
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
+	// return result
+}
 
 //factorial of 5 => 5*4*3*2*1 => 120

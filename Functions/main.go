f1dc4b3c34ae31d153cf9b9024beaa7c299fa330
Travis Lamberte <travis@lamberte.com>
2024-05-13 19:34:50 -0700

    recursive func

--- Functions/main.go
+++ Functions/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+	fact := factorial((5))
+	fmt.Println(fact)
 
 }
+
+
+
+
+// func factorial(number int) int {
+// 	result := 1
+// 	for i := 1; i <= number; i++ {
+// 		result = result * i
+// 	}
+// 	return result
+// }
+
+//factorial of 5 => 5*4*3*2*1 => 120

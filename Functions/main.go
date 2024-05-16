621e940b72e1694c7a012ccc97a768117e1ca486
Travis Lamberte <travis@lamberte.com>
2024-05-16 07:51:07 -0700

    add comments

--- Functions/main.go
+++ Functions/main.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func main() {
 
+	// call func with slice
 	numbers := []int{1, 2, 3, 5, 4, 5, 6}
 	sum := sumup(numbers)
 	fmt.Println(sum)
 	
-	sumVariatic := sumupVariatic(1, 2, 3)
+	// call variatic func with multiple numbers
+	sumVariatic := sumupVariatic(1, 2, 3, 4, 5)
 	fmt.Println(sumVariatic)
 
+	// call variatif func with slice
 	sumCallVariaticWithSlice := sumupVariatic(numbers...)
 	fmt.Println(sumCallVariaticWithSlice)
 

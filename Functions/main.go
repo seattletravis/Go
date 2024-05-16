4c96c90b5c1a9e96f219b0f6e99a676f5966a2b6
Travis Lamberte <travis@lamberte.com>
2024-05-16 07:41:38 -0700

    create variatic version of sumup func

--- Functions/main.go
+++ Functions/main.go
@@ -5,14 +5,27 @@ import "fmt"
 func main() {
 
 	numbers := []int{1, 2, 3, 5, 4, 5, 6}
-
 	sum := sumup(numbers)
-
 	fmt.Println(sum)
+	
+	sumVariatic := sumup()
+	fmt.Println(sumVariatic)
 
 }
 
+// function that takes array of ints
 func sumup(numbers []int) int {
+	sum := 0
+
+	for _, val := range numbers {
+		sum += val
+	}
+
+	return sum
+}
+
+// variatic version of function
+func sumupVariatic(numbers ...int) int {
 	sum := 0
 
 	for _, val := range numbers {

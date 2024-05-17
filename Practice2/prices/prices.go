7a60e55cca41225d699d51d4a9c66b69c37bb60c
Travis Lamberte <travis@lamberte.com>
2024-05-17 10:35:04 -0700

    finish constructor

--- Practice2/prices/prices.go
+++ Practice2/prices/prices.go
@@ -5,3 +5,10 @@ type TaxIncludedPriceJob struct {
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}

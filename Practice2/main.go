3330a0bd81340d36556ba3501194db724853d067
Travis Lamberte <travis@lamberte.com>
2024-05-17 10:17:30 -0700

    starting code

--- Practice2/main.go
+++ Practice2/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello World")
+	prices := []float64{10, 20, 30}
+	taxRate := []float64{0, 0.7, 0.1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRate {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
 }

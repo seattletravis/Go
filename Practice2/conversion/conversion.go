b676a7bba3601a96d347495afc416b945ec62c3e
Travis Lamberte <travis@lamberte.com>
2024-05-23 16:49:26 -0700

    fix error message

--- Practice2/conversion/conversion.go
+++ Practice2/conversion/conversion.go
@@ -8,12 +8,12 @@ import (
 func StringsToFloat(strings []string) ([]float64, error) {
 	var floats []float64
 
-	for stringIndex, stringVal := range strings {
+	for _, stringVal := range strings {
 		floatVal, err := strconv.ParseFloat(stringVal, 64)
 
 		if err != nil {
 
-			return nil, errors.New("Failed to convert string to float.")
+			return nil, errors.New("failed to convert string to float")
 		}
 
 		floats = append(floats, floatVal)

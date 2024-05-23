4637eed380c5cc4cc5412ff53b57dfcf72c276d4
Travis Lamberte <travis@lamberte.com>
2024-05-23 16:44:51 -0700

    refactor

--- Practice2/conversion/conversion.go
+++ Practice2/conversion/conversion.go
@@ -6,8 +6,8 @@ import (
 )
 
 func StringsToFloat(strings []string) ([]float64, error) {
-	for stringIndex, string := range strings {
-		floatPrice, err := strconv.ParseFloat(line, 64)
+	for stringIndex, stringVal := range strings {
+		floatPrice, err := strconv.ParseFloat(stringVal, 64)
 		if err != nil {
 			fmt.Println("Could not parse data to float64.")
 			fmt.Println(err)
